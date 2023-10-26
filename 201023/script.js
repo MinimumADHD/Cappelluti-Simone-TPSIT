a5d7c2674785a30e72f765e78ac40b875e8432e5
MinimumADHD <simone.cappe06@gmail.com>
2023-10-26 23:56:11 +0200

    Ae

--- 201023/script.js
+++ 201023/script.js
@@ -1,5 +1,10 @@
 // Cappelluti Simone 4Di
 
+let submitNumButton = document.getElementById("submitNumButton");
+let inputNumOne = document.getElementById("numInput_one");
+let inputNumTwo = document.getElementById("numInput_two");
+let resultLabel = document.getElementById("resultLabel");
+
 // Converting any given int (for example I used 14 (01110) for testing) into its octal binary relative
 function convertNumberToBin(numberToConvert) {
     let binRapresentationAsString = "";
@@ -25,4 +30,56 @@ function convertIntToSeqArray(intToConvert) {
     return returnable;
 }
 
+// Calc AND
+function calcAND(numParamOne, numParamTwo) {
+    let returnable = [];
+    let binNumOne = convertIntToSeqArray(convertNumberToBin(numParamOne));
+    let binNumTwo = convertIntToSeqArray(convertNumberToBin(numParamTwo));
+
+    for (let i = 0; i < binNumOne.length; i++) {
+        returnable.push(binNumOne.at(i) && binNumTwo.at(i));
+    }
+
+    return returnable;
+}
+
+// Calc OR
+function calcOR(numParamOne, numParamTwo) {
+    let returnable = [];
+    let binNumOne = convertIntToSeqArray(convertNumberToBin(numParamOne));
+    let binNumTwo = convertIntToSeqArray(convertNumberToBin(numParamTwo));
+
+    for (let i = 0; i < binNumOne.length; i++) {
+        returnable.push(binNumOne.at(i) || binNumTwo.at(i));
+    }
+
+    return returnable;
+}
+
+// Calc XOR
+function calcXOR(numParamOne, numParamTwo) {
+    let returnable = [];
+    let binNumOne = convertIntToSeqArray(convertNumberToBin(numParamOne));
+    let binNumTwo = convertIntToSeqArray(convertNumberToBin(numParamTwo));
+
+    for (let i = 0; i < binNumOne.length; i++) {
+        returnable.push(binNumOne.at(i) ^ binNumTwo.at(i));
+    }
+
+    return returnable;
+}
+
+function onSubmit() {
+    let NumOne = inputNumOne.value
+    let NumTwo = inputNumTwo.value
+
+    let andVal = calcAND(NumOne, NumTwo);
+    let orVal = calcOR(NumOne, NumTwo);
+    let xorVal = calcXOR(NumOne, NumTwo);
+
+    resultLabel.innerHTML = `RISULTATI: \n AND; ${andVal}\n OR; ${orVal}\n XOR; ${xorVal}`
+}
+
 console.log(convertIntToSeqArray(convertNumberToBin(14)));
+
+submitNumButton.onclick = () => onSubmit();
