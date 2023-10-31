d469b0089749004d26279a76805facb616a6ce01
MinimumADHD <simone.cappe06@gmail.com>
2023-10-31 10:05:58 +0100

    JS edit

---
+++ 021123/script.js
@@ -0,0 +1,19 @@
+let selectOptions = document.getElementById("locationInput");
+
+function fetchExtData (url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(error => console.error(`Error searching Italian cities. Check link ${url}...`))
+        return 1;
+}
+
+function onHTMLLoad () {
+
+}
+
+let json = fetchExtData("giagia");
+
+onHTMLLoad();
