e8b38a7ad20cab2708ab928ef72397f9fd497560
MinimumADHD <simone.cappe06@gmail.com>
2023-11-03 00:25:40 +0100

    FETCH implementation

--- 021123/script.js
+++ 021123/script.js
@@ -1,19 +1,34 @@
-// let selectOptions = document.getElementById("locationInput");
+let selectOptions = document.getElementById("locationInput");
 
-function fetchExtData (url) {
-    fetch(url)
+function fetchExtData(url) {
+    return fetch(url)
         .then(response => response.json())
+        .catch(error => console.error(`Error fetching data from ${url}:`, error));
+}
+
+function populateSelectWithJSON(url_json, select_element) {
+    fetchExtData(url_json)
         .then(data => {
-            return data;
-        })
-        .catch(error => console.error(`Error searching Italian cities. Check link ${url}...`))
-        return 1;
+            for (let region in data) {
+                if (data.hasOwnProperty(region)) {
+                    let optgroup = document.createElement('optgroup');
+                    optgroup.label = region;
+
+                    data[region].forEach(city => {
+                        let option = document.createElement("option");
+                        option.value = city;
+                        option.textContent = city;
+                        optgroup.appendChild(option);
+                    });
+
+                    select_element.appendChild(optgroup);
+                }
+            }
+        });
 }
 
-function onHTMLLoad () {
-    
+function onHTMLLoad (guh) {
+    populateSelectWithJSON("https://raw.githubusercontent.com/MinimumADHD/Cappelluti-Simone-TPSIT/main/cities.json", selectOptions)
 }
 
-let json = fetchExtData("giagia");
-
-onHTMLLoad();
+onHTMLLoad()
