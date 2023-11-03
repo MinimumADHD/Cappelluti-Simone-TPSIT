4fc0c49a6ae5a9efdf95a946be10e253f87e9ebf
MinimumADHD <simone.cappe06@gmail.com>
2023-11-03 01:01:56 +0100

    Finished "Anagrafe"

--- 021123/script.js
+++ 021123/script.js
@@ -1,4 +1,24 @@
 let selectOptions = document.getElementById("locationInput");
+let confirmButton = document.getElementById("confirmButton");
+let headerToChange = document.getElementById("headerToChange");
+
+let nameInput = document.getElementById("nameInput");
+let lastNameInput = document.getElementById("lastNameInput");
+let birthdayInput = document.getElementById("birthdayInput");
+let genderInput = document.getElementById("genderForm");
+
+function calcAge(birthday) {
+    let tDay = new Date();
+    let birthDate = new Date(birthday);
+
+    let age = tDay.getFullYear() - birthDate.getFullYear();
+
+    if (tDay.getMonth() < birthDate.getMonth() || (tDay.getMonth() === birthDate.getMonth() && tDay.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    return age;
+}
 
 function fetchExtData(url) {
     return fetch(url)
@@ -28,7 +48,19 @@ function populateSelectWithJSON(url_json, select_element) {
 }
 
 function onHTMLLoad (guh) {
-    populateSelectWithJSON("https://raw.githubusercontent.com/MinimumADHD/Cappelluti-Simone-TPSIT/main/cities.json", selectOptions)
+    populateSelectWithJSON("https://raw.githubusercontent.com/MinimumADHD/Cappelluti-Simone-TPSIT/main/cities.json", selectOptions);
+}
+
+function getGender(genderForm) {
+    let selected = genderForm.querySelector('input[name="gender"]:checked');
+
+    return selected.value;
+}
+
+function onConfirmClick() {
+    headerToChange.innerHTML = `RISULTATO: ${lastNameInput.value} ${nameInput.value}, ${calcAge(birthdayInput.value)} anni, nato il ${birthdayInput.value}, residente a ${selectOptions.value}, Sesso: ${getGender(genderInput)}`;
 }
 
 onHTMLLoad()
+
+confirmButton.onclick = () => onConfirmClick();
