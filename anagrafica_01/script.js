4984ddcc6e05c914d86a3eb42383a380ca9dd503
MinimumADHD <91323541+MinimumADHD@users.noreply.github.com>
2023-12-17 11:30:13 +0100

    *

---
+++ anagrafica_01/script.js
@@ -0,0 +1,7 @@
+function uploadDataToStorage(...options) {
+    for (i = 0; i < options.length; i++) {
+        console.log[options[i]]
+    }
+}
+
+uploadDataToStorage("Giorgio", "Astarito", "Mario")
