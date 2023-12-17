26cdd863689af7f62d36ac505727a33a9c2aea01
MinimumADHD <91323541+MinimumADHD@users.noreply.github.com>
2023-12-17 13:38:20 +0100

    *

--- anagrafica_01/scriptOutput.js
+++ anagrafica_01/scriptOutput.js
@@ -1,14 +1,18 @@
 let datiInInput = JSON.parse(localStorage.getItem("anagraficaStorage"));
-let testParagraph = document.getElementById("testParagraph");
+let nameParagraph = document.getElementById("nameParagraph")
+let lastNameParagraph = document.getElementById("lastNameParagraph")
 
 if (datiInInput) {
-    for (let key in datiInInput) {
-        if (datiInInput.hasOwnProperty(key)) {
-            testParagraph.innerHTML += " " + datiInInput[key];
+    for (let [key, value] of Object.entries(datiInInput)) {
+        console.log(key + "" + value)
+        // Use 'key' and 'value' as needed
+        if (key === "0") {
+            nameParagraph.innerHTML = "NOME RICEVUTO: " + value
+        } else if (key == "1") {
+            lastNameParagraph.innerHTML = "COGNOME RICEVUTO: " + value
         }
     }
     localStorage.clear();
 } else {
     console.error("NO DATA FOUND FROM THE PREVIOUS PAGE... DID YOU DO EVERYTHING CORRECTLY?");
-    localStorage.clear()
 }
