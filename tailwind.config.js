b912de44bc795c38f6acbcf219fbe025a963a1c2
minimumadhd <simone.cappe06@gmail.com>
2024-03-19 09:57:48 +0100

    Update index.html with new styles and links

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{css,html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'fira': ['Fira Code'],
+        'spacemono': ['Space Mono']
+      }
+    },
+  },
+  plugins: [],
+}
+
